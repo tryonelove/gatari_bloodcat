ab143f360d50e02f86198d4f6b421d95fd9fc43c
tryonelove <try13crew@gmail.com>
2018-07-27 04:08:07 +0300

    Add files via upload

---
+++ gatari-bloodcat.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name bloodcat for gatari
+// @description Simple Bloodcat button for gatari
+// @author tryonelove
+// @version 1.0
+// @include http*://osu.gatari.pw/b/*
+// @include http*://osu.gatari.pw/s/*
+// ==/UserScript==
+
+var result = null,
+    mapID = null,
+    mapsetID = null;
+
+function init(){
+    var temp;
+    temp = $(".beatmapTab.selected").attr("href").split("&")[0].split("/");
+    mapID = temp[temp.length - 1];
+    temp = $(".beatmap-preview").attr("src").split("/");
+    mapsetID = temp[temp.length - 1].split("l")[0];
+    addBloodcatMirror();
+}
+function addBloodcatMirror(){
+    if(mapsetID !== null){
+        $(".beatmap-downlaod").first().before(
+            $("<div>").addClass("beatmap-downlaod").append(
+                $("<a>").attr("href", "http://bloodcat.com/osu/s/" + mapsetID).addClass("btn btn-blue").text("Bloodcat")
+            )
+        );
+        // old mirror
+        //$(".beatmap_download_link").last().after("<br>",$("<a></a>").text("Bloodcat mirror").attr("href", "http://bloodcat.com/osu/s/" + mapsetID))
+    }
+}
+$(document).ready(function(){
+    init()
+});
